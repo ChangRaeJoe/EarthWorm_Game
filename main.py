9a10d5050599c04c0e7f91baf8b14880dc95a677
Changrae <ckdfo1995@naver.com>
2018-07-02 14:02:05 +0900

    class add, 2player add

--- main.py
+++ main.py
@@ -2,198 +2,157 @@ import pygame as pg
 import constants as c
 import item, user
 from time import sleep
-import random
 
-def edged():
-    direction = [pg.K_DOWN, pg.K_LEFT, pg.K_UP, pg.K_RIGHT]
-    dir_choice = random.choice(direction)
-    return dir_choice
+class Arena:
+    def __init__(self):
+        pg.init()
+        pg.display.set_caption('EarthWorm Game()')
+        self.screen = pg.display.set_mode((c.FULL_WIDTH, c.FULL_HEIGHT))
+        self.clock = pg.time.Clock()
+        self.game_initing()
 
-def limit(xy):
-    global speed
-    x = xy[0]
-    y = xy[1]
-    tmp_key = xy[2]
-    if c.WIDTH - c.RECT_WIDTH < x: #막는 동시에 방향을 바꿔준다.
-        x -= c.SPEED + speed
-        tmp_key = edged()
-    elif 0>x:
-        x = 0
-        tmp_key = edged()
+    def game_initing(self):
+        self.init_x = 0
+        self.init_y = 0
+        self.change_width = 0
+        self.change_height = 0
 
-    if c.HEIGHT - c.RECT_HEIGHT< y:
-        y -= c.SPEED + speed
-        tmp_key = edged()
-    elif 0>y:
-        y = 0
-        tmp_key = edged()
+        # 아이템 생성
+        self.items = item.item_init()
+        # 값들 변경
+        self.smile = False
+        self.speed = 0
+        self.score = 0
 
-    return (x, y, tmp_key)
+    def menu(self):
+        pg.draw.line(self.screen, c.RED, (c.WIDTH, 0), (c.WIDTH, c.HEIGHT), 1)
 
-def auto_moved(box_one):
-    global speed
-    x = box_one[0]
-    y = box_one[1]
-    keys = box_one[2]
+        score_str = "score: " + str(self.score)
+        time_str = "time: " + str(int(pg.time.get_ticks() / 1000)) + "s"
+        speed_str = "speed: " + str(self.speed)
+        score_pos = (c.WIDTH + 10, 10)
+        time_pos = (c.WIDTH + 10, 30)
+        speed_pos = (c.WIDTH + 10, 50)
 
-    if keys == pg.K_RIGHT:
-        x += c.SPEED + speed
-    elif keys == pg.K_LEFT:
-        x -= c.SPEED + speed
-    elif keys == pg.K_UP:
-        y -= c.SPEED + speed
-    elif keys == pg.K_DOWN:
-        y += c.SPEED + speed
-    return (x, y)
+        font = pg.font.Font('freesansbold.ttf', 15)
+        text = font.render(score_str, True, c.RED)
+        text2 = font.render(time_str, True, c.RED)
+        text3 = font.render(speed_str, True, c.RED)
+
+        self.screen.blit(text, score_pos)
+        self.screen.blit(text2, time_pos)
+        self.screen.blit(text3, speed_pos)
+
+    def textObj(self, text, font):
+        textSurface = font.render(text, True, c.RED)
+        return textSurface, textSurface.get_rect()
+
+    def dispMessage(self, text):
+        largeText = pg.font.Font('freesansbold.ttf', 115)
+        TextSurf, TextRect = self.textObj(text, largeText)
+        TextRect.center = ((c.WIDTH / 2), (c.HEIGHT / 2))
+        self.screen.blit(TextSurf, TextRect)
+        pg.display.update()
+        sleep(2)
+        self.game_initing()
 
 
-def time_attack():
-    global speed
-    min = 60
-    tick = pg.time.get_ticks()
-    # +1 2 3 4 6 8 10 13
-    # item add <- speed up !!
-    if tick/1000 < min:
-        speed = 0
-    elif tick/1000 < min*1.2:
-        speed = 1
-    elif tick / 1000 < min*1.5:
-        speed = 2
-    elif tick / 1000 < min*1.7:
-        speed = 3
-    elif tick / 1000 < min*1.8:
-        speed = 4
-    elif tick / 1000 < min*2:
-        speed = 6
-    elif tick / 1000 < min*2.2:
-        speed = 8
-    else:
-        speed = 10
 
-def textObj(text, font):
-    textSurface = font.render(text, True, c.RED)
-    return textSurface,textSurface.get_rect()
+    def time_attack(self):
+        min = 60
+        tick = pg.time.get_ticks()
+        # +1 2 3 4 6 8 10 13
+        # item add <- speed up !!
+        if tick / 1000 < min:
+            self.speed = 0
+        elif tick / 1000 < min * 1.2:
+            self.speed = 1
+        elif tick / 1000 < min * 1.5:
+            self.speed = 2
+        elif tick / 1000 < min * 1.7:
+            self.speed = 3
+        elif tick / 1000 < min * 1.8:
+            self.speed = 4
+        elif tick / 1000 < min * 2:
+            self.speed = 6
+        elif tick / 1000 < min * 2.2:
+            self.speed = 8
+        else:
+            self.speed = 10
 
-def dispMessage(text):
-    global screen
-
-    largeText = pg.font.Font('freesansbold.ttf', 115)
-    TextSurf, TextRect = textObj(text, largeText)
-    TextRect.center = ((c.WIDTH/2), (c.HEIGHT/2))
-    screen.blit(TextSurf, TextRect)
-    pg.display.update()
-    sleep(2)
-    game_initing()
-
-def menu():
-    global screen, score,speed
-    pg.draw.line(screen, c.RED, (c.WIDTH,0), (c.WIDTH, c.HEIGHT), 1)
-
-    score_str = "score: " + str(score)
-    time_str = "time: " + str(int(pg.time.get_ticks()/1000)) + "s"
-    speed_str = "speed: " + str(speed)
-    score_pos = (c.WIDTH + 10, 10)
-    time_pos = (c.WIDTH + 10, 30)
-    speed_pos = (c.WIDTH + 10, 50)
-
-    font = pg.font.Font('freesansbold.ttf', 15)
-    text = font.render(score_str, True, c.RED)
-    text2 = font.render(time_str, True, c.RED)
-    text3 = font.render(speed_str, True, c.RED)
-
-    screen.blit(text, score_pos)
-    screen.blit(text2, time_pos)
-    screen.blit(text3, speed_pos)
-
-def game_initing():
-    global init_x, init_y, change_width, change_height, earth_w, items, smile, speed, score
-    init_x = 0
-    init_y = 0
-    change_width = 0
-    change_height = 0
-    # 지렁이 생성[x, y, direction]
-    earth_w = []
-    for i in range(0, 3):
-        earth_w.append([c.RECT_X, c.RECT_Y, 0])
-    # 아이템 생성
-    items = item.item_init()
-    # 값들 변경
-    smile = False
-    speed = 0
-    score = 0
+    def get_screen(self):
+        return self.screen
+    def get_item(self):
+        return self.items
+    def get_score(self):
+        return self.score
+    def set_score(self, score):
+        self.score = score
+    def set_FPS(self, fps):
+        self.clock.tick(c.SCREEN_FPS)
+    def get_speed(self):
+        return self.speed
 
 if __name__ == '__main__':
-    pg.init()
-    pg.display.set_caption('EarthWorm Game()')
-    screen = pg.display.set_mode((c.FULL_WIDTH, c.FULL_HEIGHT))
-    clock = pg.time.Clock()
-    init_x = 0
-    init_y = 0
-    change_width = 0
-    change_height = 0
-    #지렁이 생성[x, y, direction]
-    earth_w = []
-    for i in range(0, 3):
-        earth_w.append([c.RECT_X, c.RECT_Y, 0])
-    #아이템 생성
-    items = item.item_init()
-    #값들 변경
-    smile = False
-    speed = 0
-    score = 0
-    one = user.User(earth_w, items)
-    while not smile:
-        #키 동작
+    arena = Arena()
+    screen = arena.get_screen()
+    tmp_keys = [pg.K_RIGHT, pg.K_LEFT, pg.K_UP, pg.K_DOWN]
+    tmp_keys2 = [pg.K_d, pg.K_a, pg.K_w, pg.K_s]
+    # 2 player
+    player = []
+    player.append(user.User(tmp_keys, arena.get_item()))
+    player.append(user.User(tmp_keys2, arena.get_item()))
+
+    while not arena.smile:
+        items = arena.get_item()
+        player[0].set_item(items)
+        player[1].set_item(items)
+        ##요기에 1p만 되는 이유가???
+        # Press Keyboard
         for event in pg.event.get():
-            if event.type == pg.QUIT:
-                smile = True
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_RIGHT:
-                    earth_w[0][2] = pg.K_RIGHT
-                elif event.key == pg.K_LEFT:
-                    earth_w[0][2] = pg.K_LEFT
-                elif event.key == pg.K_UP:
-                    earth_w[0][2] = pg.K_UP
-                elif event.key == pg.K_DOWN:
-                    earth_w[0][2] = pg.K_DOWN
-        #moving: pre value->next value
-        for index, xy in enumerate(earth_w):
-            next = index+1
-            if index == 0:
-                cur_x, cur_y = earth_w[index][0], earth_w[index][1]
-                pre_x = earth_w[next][0]
-                pre_y = earth_w[next][1]
-                earth_w[next][0], earth_w[next][1] = cur_x, cur_y
-                earth_w[index][0], earth_w[index][1] = auto_moved(xy)
-                earth_w[index][0], earth_w[index][1], earth_w[index][2] = limit(xy)
-            elif index < len(earth_w)-1:
-                cur_x, cur_y = pre_x, pre_y
-                pre_x = earth_w[next][0]
-                pre_y = earth_w[next][1]
-                earth_w[next][0], earth_w[next][1] = cur_x, cur_y
-        one.set_data(earth_w, items)
-        #item을 먹었을때의 동작
-        score += 10*one.eat()
-        #crashed
-        if one.crash():
-            dispMessage('Crasheed!!')
+            for i in range(0, len(player)):
+                earth_w = player[i].get_earwh_w()
+                tmp_key = player[i].get_key()
+                if event.type == pg.QUIT:
+                    arena.smile = True
+                if event.type == pg.KEYDOWN:
+                    if event.key == tmp_key[0]:
+                        earth_w[0][2] = tmp_key[0]
+                    elif event.key == tmp_key[1]:
+                        earth_w[0][2] = tmp_key[1]
+                    elif event.key == tmp_key[2]:
+                        earth_w[0][2] = tmp_key[2]
+                    elif event.key == tmp_key[3]:
+                        earth_w[0][2] = tmp_key[3]
+
+        # Moving: pre value->next value
+        for i in range(0, len(player)):
+            player[i].Moving(arena.get_speed())
+        for i in range(0, len(player)):
+            # Eating item
+            tmp_score = arena.get_score() + 10 * player[i].eat()
+            arena.set_score(tmp_score)
+            # crashed
+            if player[i].crash():
+                arena.dispMessage('Crasheed!!')
+
         #time attack - speed
-        time_attack()
-        #print(speed)
-        #화면그리기
+        arena.time_attack()
+        #Drawing screen, items, eartwh_worm
         screen.fill(0)
-        menu()
-        ##아이템 그리기
+        arena.menu()
         for index, xy in enumerate(items):
             pg.draw.rect(screen, c.BLUE, (xy[0], xy[1], c.RECT_WIDTH, c.RECT_HEIGHT))
-        ##지렁이 그리기
-        for index, xy in enumerate(earth_w):
-            if index%2==0:
-                pg.draw.rect(screen, c.GREEN, (earth_w[index][0], earth_w[index][1], c.RECT_WIDTH, c.RECT_HEIGHT))
-            else:
-                pg.draw.rect(screen, c.RED, (earth_w[index][0], earth_w[index][1], c.RECT_WIDTH, c.RECT_HEIGHT))
+
+        for i in range(0, len(player)):
+            earth_w = player[i].get_earwh_w()
+            for index, xy in enumerate(earth_w):
+                if index % 2 == 0:
+                    pg.draw.rect(screen, c.GREEN, (earth_w[index][0], earth_w[index][1], c.RECT_WIDTH, c.RECT_HEIGHT))
+                else:
+                    pg.draw.rect(screen, c.RED, (earth_w[index][0], earth_w[index][1], c.RECT_WIDTH, c.RECT_HEIGHT))
 
         pg.display.flip()
-        clock.tick(c.SCREEN_FPS)
-        if smile==True:
+        arena.set_FPS(c.SCREEN_FPS)
+        if arena.smile==True:
             pg.quit()
--- user.py
+++ user.py
@@ -1,14 +1,19 @@
 import pygame as pg
 import item, main
 import constants as c
-
+import random
 class User:
-    def __init__(self, earth_w, items):
-        self.set_data(earth_w, items)
-
-    def set_data(self, earth_w, items):
-        self.earth_w = earth_w
+    def __init__(self,  keys, items):
+        # 지렁이 생성[x, y, direction]
+        self.earth_w_init()
         self.items = items
+        self. keys = keys
+
+
+    def earth_w_init(self):
+        self.earth_w = []
+        for i in range(0, 3):
+            self.earth_w.append([c.RECT_X, c.RECT_Y, 0])
 
     def eat(self):
         for i, xy in enumerate(self.earth_w):
@@ -52,10 +57,73 @@ class User:
                 # 2. xy[0] is small than mn's x
                 if xy[1] >= mn[1] and xy[1] <= mn[1] + c.RECT_HEIGHT:
                     if xy[0] >= mn[0] and xy[0] <= mn[0] + c.RECT_WIDTH:
+                        self.earth_w_init()
                         return 1
         return 0
 
+    def Moving(self, speed):
+        self.speed = speed
+        earth_w = self.get_earwh_w()
+        for index, xy in enumerate(earth_w):
+            next = index + 1
+            if index == 0:
+                cur_x, cur_y = earth_w[index][0], earth_w[index][1]
+                pre_x = earth_w[next][0]
+                pre_y = earth_w[next][1]
+                earth_w[next][0], earth_w[next][1] = cur_x, cur_y
+                earth_w[index][0], earth_w[index][1] = self.auto_moved(xy)
+                earth_w[index][0], earth_w[index][1], earth_w[index][2] = self.limit(xy)
+            elif index < len(earth_w) - 1:
+                cur_x, cur_y = pre_x, pre_y
+                pre_x = earth_w[next][0]
+                pre_y = earth_w[next][1]
+                earth_w[next][0], earth_w[next][1] = cur_x, cur_y
 
+    def edged(self):
+        dir_choice = random.choice(self.keys)
+        return dir_choice
+
+    def limit(self, xy):
+        x = xy[0]
+        y = xy[1]
+        tmp_key = xy[2]
+        if c.WIDTH - c.RECT_WIDTH < x:  # 막는 동시에 방향을 바꿔준다.
+            x -= c.SPEED + self.speed
+            tmp_key = self.edged()
+        elif 0 > x:
+            x = 0
+            tmp_key = self.edged()
+
+        if c.HEIGHT - c.RECT_HEIGHT < y:
+            y -= c.SPEED + self.speed
+            tmp_key = self.edged()
+        elif 0 > y:
+            y = 0
+            tmp_key = self.edged()
+
+        return (x, y, tmp_key)
+
+    def auto_moved(self, box_one):
+        x = box_one[0]
+        y = box_one[1]
+        key = box_one[2]
+
+        if key == self.keys[0]:
+            x += c.SPEED + self.speed
+        elif key == self.keys[1]:
+            x -= c.SPEED + self.speed
+        elif key == self.keys[2]:
+            y -= c.SPEED + self.speed
+        elif key == self.keys[3]:
+            y += c.SPEED + self.speed
+        return (x, y)
+
+    def get_earwh_w(self):
+        return self.earth_w
+    def get_key(self):
+        return self.keys
+    def set_item(self, item):
+        self.items = item
 
 
 
